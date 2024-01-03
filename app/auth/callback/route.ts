ab2cb96ab263f6a6e35fec51ec84930be8f47480
arthur-upton <arthur.upton@eliteitteam.com>
2024-01-03 20:49:40 +0500

    feat: insert user to database upon auth verification

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,15 +6,26 @@ export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the Auth Helpers package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get('code');
 
   if (code) {
-    const cookieStore = cookies()
-    const supabase = createClient(cookieStore)
-    await supabase.auth.exchangeCodeForSession(code)
+    const cookieStore = cookies();
+    const supabase = createClient(cookieStore);
+    await supabase.auth.exchangeCodeForSession(code);
+
+    const { data: { user } } = await supabase.auth.getUser();
+    console.log(user);
+
+    if (user) {
+      // what if this errors ??
+      const { error } = await supabase.from('user').insert({
+        user_id: user.id,
+        email: user.email
+      });
+    }
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin)
+  return NextResponse.redirect(requestUrl.origin);
 }
