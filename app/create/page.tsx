5bac099975247674bf3b3250fefec40e05b7428e
arthur-upton <arthur.upton@eliteitteam.com>
2024-01-03 23:10:11 +0500

    feat: create form inputs for event creation

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -72,15 +72,121 @@ export default function Create({
             required
           /> */}
 
-<label className="text-lg font-semibold" htmlFor="email">
-            Email
+          <label className="text-lg font-semibold" htmlFor="title">
+            Title
           </label>
           <input
-            className="rounded-md px-4 py-2 bg-inherit border-2 mb-6 placeholder-white placeholder-opacity-50 focus:outline-purple-300"
-            name="email"
-            placeholder="you@example.com"
+            className="rounded-md px-4 py-2 bg-inherit border-2 border-white/25 mb-2 placeholder-white placeholder-opacity-50 focus:outline-purple-300"
+            name="title"
+            placeholder="Future Fest"
+            required
+            type="text"
+          />
+
+          <label className="text-lg font-semibold" htmlFor="venue">
+            Venue
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border-2 border-white/25 mb-2 placeholder-white placeholder-opacity-50 focus:outline-purple-300"
+            name="venue"
+            placeholder="Expo Center, Lahore"
+            required
+            type="text"
+          />
+          {/* 
+          <div className="flex justify-around border border-red-500">
+            <input
+              className=""
+              name="eventFormRadio"
+              placeholder="..."
+              required
+              type="radio"
+              id="remote"
+            />
+            <label className="text-lg font-semibold" htmlFor="remote">
+              Remote
+            </label>
+            <input
+              className=""
+              name="eventFormRadio"
+              placeholder="..."
+              required
+              type="radio"
+              id="onsite"
+            />
+            <label className="text-lg font-semibold" htmlFor="onsite">
+              Onsite
+            </label>
+          </div> */}
+
+          <div className="grid grid-cols-2 gap-2 rounded-md bg-inherit border-2 border-white/25 mb-2 p-1">
+            <div>
+              <input type="radio" name="venueType" id="onsite" value="onsite" className="peer hidden" defaultChecked />
+              <label htmlFor="onsite" className="block cursor-pointer select-none rounded-md px-3 py-2 text-center peer-checked:bg-purple-500/75 peer-checked:font-bold peer-checked:text-white">Onsite</label>
+            </div>
+
+            <div>
+              <input type="radio" name="venueType" id="remote" value="remote" className="peer hidden" />
+              <label htmlFor="remote" className="block cursor-pointer select-none rounded-md px-3 py-2 text-center peer-checked:bg-purple-500/75 peer-checked:font-bold peer-checked:text-white">Remote</label>
+            </div>
+          </div>
+
+          <label className="text-lg font-semibold" htmlFor="link">
+            Link
+          </label>
+          <input
+            disabled
+            className="disabled:bg-slate-600/20 rounded-md px-4 py-2 bg-inherit border-2 border-white/25 mb-2 placeholder-white placeholder-opacity-50 focus:outline-purple-300"
+            name="link"
+            placeholder="https://meet.google.com/abc-defg-hij"
+            required
+            type="url"
+          />
+
+          <label className="text-lg font-semibold" htmlFor="description">
+            Description
+          </label>
+          <textarea
+            className="rounded-md px-4 py-2 bg-inherit border-2 border-white/25 mb-2 placeholder-white placeholder-opacity-50 focus:outline-purple-300"
+            name="description"
+            placeholder="Describe the event"
             required
           />
+
+          <label className="text-lg font-semibold" htmlFor="date">
+            Date
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border-2 border-white/25 mb-2 placeholder-white placeholder-opacity-50 focus:outline-purple-300"
+            name="date"
+            placeholder="..."
+            required
+            type="date"
+          />
+
+          <label className="text-lg font-semibold" htmlFor="time">
+            Time
+          </label>
+          <input
+            className="rounded-md px-4 py-2 bg-inherit border-2 border-white/25 mb-2 placeholder-white placeholder-opacity-50 focus:outline-purple-300"
+            name="time"
+            placeholder="..."
+            required
+            type="time"
+          />
+          {/* <div key={option.value}>
+          <input
+            type="radio"
+            id={option.value}
+            name="dynamicRadio"
+            value={option.value}
+            checked={selectedOption === option.value}
+            onChange={handleOptionChange}
+          />
+          <label htmlFor={option.value}>{option.label}</label>
+        </div> */}
+
+
           {/* month
           <input
             type="month"
